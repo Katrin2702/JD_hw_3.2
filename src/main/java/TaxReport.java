bf0968189cc68ec846ca8e68957d578d3f3ddd2f
Ekaterina Kovalenko <katrin.kovali2702@gmail.com>
2022-05-23 21:35:40 +0300

    revision

--- src/main/java/TaxReport.java
+++ src/main/java/TaxReport.java
@@ -2,7 +2,7 @@ import java.util.concurrent.atomic.LongAdder;
 
 public class TaxReport {
 
-    LongAdder report = new LongAdder();
+    private LongAdder report = new LongAdder();
 
     public void add(int sale) {
         report.add(sale);
